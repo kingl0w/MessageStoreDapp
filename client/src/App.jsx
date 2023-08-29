c94e12e8802b4a127b9f39ffee8af3064a94ba7d
kingl0w <tdytomsawyer@protonmail.com>
2023-08-29 16:17:22 -0700

    client logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  const [account, setAccount] = useState('Not Connected');
+
+  useEffect(() => {
+    const template = async () => {
+      const contractAddress = '';
+      const contractABI = '';
+      //metamask logic
+
+      try {
+        const { ethereum } = window;
+
+        const account = await ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+
+        setAccount(account);
+        const provider = new ethers.providers.Web3Provider(ethereum); //read the blockchain
+        const signer = provider.getSigner(); //write the blockchain
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        setState({ provider, signer, contract });
+      } catch (error) {
+        alert(error);
+      }
+    };
+    template();
+  }, []);
+
   return (
     <>
       <div className='App'></div>
